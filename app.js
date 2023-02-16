48901e262c66f16da2d9fded8437f437ccc17532
Gustavo <gustavomedeiros15@outlook.com.br>
2023-02-16 16:02:38 -0300

    Commit express -  Rotas e Json

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require('express')
 
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("<h1>To do Lista</h1>")
+})
+
+app.get('/json', (req, res) => {
+    res.json({title: "comprar cafe", done: "true"})
+})
+
+app.listen(3000, () => {
+    console.log("listening on http://localhost:3000")
+})
