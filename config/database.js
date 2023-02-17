e0dd31a65655597daab6af491a072d771abb6ce9
Gustavo <gustavomedeiros15@outlook.com.br>
2023-02-17 19:16:37 -0300

    Node - MongoDB adicionado, criação dos MODELS (Checklist e Task) e alteração na rota POST, tendo agora ligação com os Models

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.set("strictQuery", true)
+
+mongoose.connect('mongodb://127.0.0.1/todo-list', { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() =>console.log('Connected to MongoDB'))
+.catch((err) => console.error(err))
